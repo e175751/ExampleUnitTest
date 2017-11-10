793ca49b53e4b8847e591739f786ae6791284c7b
e175751 <e175751 e175751@ie.u-ryukyu.ac.jp>
2017-11-10 19:05:19 +0900

    first commit

--- src/test/jp/ac/uryukyu/ie/e175751/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e175751/EnemyTest.java
@@ -6,15 +6,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EnemyTest {
     @Test
-    int heroHP = 10;
-    Hero hero = new Hero("テスト勇者", heroHP, 5);
-    Enemy enemy = new Enemy("テストスライム", 6, 3);
-    enemy.dead = true;
-        for(int i=0; i<10; i++) {
-        enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
-    }
-    assertEquals(heroHP, hero.hitPoint);
     void attack() {
+        int heroHP = 10;
+        Hero hero = new Hero("テスト勇者", heroHP, 5);
+        Enemy enemy = new Enemy("テストスライム", 6, 3);
+        enemy.dead = true;
+        for(int i=0; i<10; i++) {
+            enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
+        }
+        assertEquals(heroHP, hero.hitPoint);
     }
 
 }
